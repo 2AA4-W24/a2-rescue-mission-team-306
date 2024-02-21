fcc92892c93fbea3b5859d016180fc36716a80b1
michaelnathanroberts <michaelnathanroberts@gmail.com>
2024-02-21 15:54:08 -0500

    Integrate components into decision-making process; closes #21

--- src/main/java/ca/mcmaster/se2aa4/island/team306/Aborter.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team306/Aborter.java
@@ -3,8 +3,6 @@ package ca.mcmaster.se2aa4.island.team306;
 public class Aborter {
     private Drone drone;
     public Map map;
-    
-    public Aborter(){}
 
     public boolean abort(){
         return true;
