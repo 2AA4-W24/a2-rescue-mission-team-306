22f8d5d9949f710a4038bac217fdec8ab903c3cc
DanteCsermak <dantecsermak@gmail.com>
2024-02-21 23:29:36 -0500

    Issue #24: Implemented distance formula in Distance.java

--- src/main/java/ca/mcmaster/se2aa4/island/team306/Distance.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team306/Distance.java
@@ -2,5 +2,9 @@ package ca.mcmaster.se2aa4.island.team306;
 
 public class Distance {
 
-    public static float calculateDistance(Coords c1, Coords c2){return 0.0f;}
+    public static float calculateDistance(Coords c1, Coords c2){
+        int dx = c2.x - c1.x;
+        int dy = c2.y - c1.y;
+        return (float) Math.sqrt(dx^2 + dy^2);
+    }
 }
