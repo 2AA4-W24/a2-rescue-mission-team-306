50431562fcdb372064326e06267c8214374f7de7
DanteCsermak <dantecsermak@gmail.com>
2024-02-24 00:00:36 -0500

    Issue #7: Added equals and hashing to Coords, setup Map structure

--- src/main/java/ca/mcmaster/se2aa4/island/team306/Tile.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team306/Tile.java
@@ -2,4 +2,27 @@ package ca.mcmaster.se2aa4.island.team306;
 
 public class Tile {
 
+    private MapValue type;
+    private Coords location;
+    private String id; 
+
+    public Tile(MapValue type, Coords location, String id){
+        this.type = type;
+        this.location = location;
+        this.id = id;
+    }
+
+    public Tile(MapValue type, Coords location){
+        this.type = type;
+        this.location = location;
+        this.id = null;
+    }
+
+    public MapValue getType(){
+        return this.type;
+    }
+
+    public Coords getLocation(){
+        return this.location;
+    }
 }
