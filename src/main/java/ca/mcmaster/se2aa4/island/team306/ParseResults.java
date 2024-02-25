dff3d185ab4026348f7ef7d0cc873fcbdc591dc5
DanteCsermak <dantecsermak@gmail.com>
2024-02-24 23:47:08 -0500

    Issue #9: added static variables for parsing logic

--- src/main/java/ca/mcmaster/se2aa4/island/team306/ParseResults.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team306/ParseResults.java
@@ -3,20 +3,23 @@ package ca.mcmaster.se2aa4.island.team306;
 import org.json.JSONObject;
 
 public class ParseResults {
-    /*
-     * Return value of ParseResults can be modified if necessary. Arbitrarily set as string
-     */
-
-    public static MapValue parsePhotoScan(String results){
-        return MapValue.OCEAN;
+    private static Direction direction;
+    private static Decision decision;
+    private static String id;
+    
+    public static ParsedResult parseResult(String results){
+        return new ParsedResult(direction, decision, results);
     }
 
-    public static ParsedRadarResult parseRadarScan(String results){
-        return new ParsedRadarResult(Direction.NORTH);
+    public static void setDirection(Direction newDirection){
+        direction = newDirection;
     }
 
-    public static int parseStatus(String results){
-        JSONObject jsonObject = new JSONObject(results);
-        return jsonObject.getInt("cost");
+    public static void setDecision(Decision newDecision){
+        decision = newDecision;
+    }
+
+    public static void setID(String newID){
+        id = newID;
     }
 }
