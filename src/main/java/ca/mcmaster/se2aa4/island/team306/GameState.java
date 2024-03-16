aecc86913fab7396ad57b489d79e8dfa59fd09ff
Oliverzzzzz <zhaoc59@mcmaster.ca>
2024-03-16 12:14:03 -0400

    create the move logic for the branch phase #58

--- src/main/java/ca/mcmaster/se2aa4/island/team306/GameState.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team306/GameState.java
@@ -1,5 +1,9 @@
 package ca.mcmaster.se2aa4.island.team306;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public enum GameState {
     SETUP, // Know our initial position in the map
     SEARCH, // Grid-search until emergency site is found
@@ -20,4 +24,41 @@ public enum GameState {
                 return this;
         }
     }
+
+    public void findNearestCreek(Drone drone, Map map, Mover mover) {
+        Coords dronePosition = drone.getPosition();
+        Direction startOrientation = drone.getHeading();
+        List<Direction> spiralDirections = generateSpiralDirections();
+        for (Direction direction : spiralDirections) {
+            Coords nextPosition = dronePosition.step(direction);
+            MapValue nextTile = map.checkCoords(nextPosition);
+            if (nextTile == MapValue.CREEK) {
+                DecisionType decisionType = DecisionType.PHOTO;
+                ParsedResult result = new ParsedResultBuilder(new Decision(decisionType, direction))
+                        .populate("{\"cost\":0,\"extras\":{\"creeks\":[\"nearest_creek_id\"]}}")
+                        .build();
+                drone.updateResult(result);
+                return;
+            }
+            mover.move();
+        }
+    }
+    
+    private List<Direction> generateSpiralDirections() {
+        List<Direction> directions = new ArrayList<>();
+        directions.add(Direction.EAST);
+        directions.add(Direction.NORTH);
+        directions.add(Direction.WEST);
+        directions.add(Direction.SOUTH);
+        int step = 1;
+        while (true) {
+            for (int i = 0; i < step; i++) directions.add(Direction.EAST);
+            for (int i = 0; i < step; i++) directions.add(Direction.NORTH);
+            step++;
+            for (int i = 0; i < step; i++) directions.add(Direction.WEST);
+            for (int i = 0; i < step; i++) directions.add(Direction.SOUTH);
+            step++;
+        }
+    }
+    
 }
