5f3d9fdaf2578203bec926e865c83763a7cc0c95
Oliverzzzzz <zhaoc59@mcmaster.ca>
2024-03-21 18:33:01 -0400

    update Javadoc for ReportGenerator

--- src/main/java/ca/mcmaster/se2aa4/island/team306/ReportGenerator.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team306/ReportGenerator.java
@@ -5,14 +5,24 @@ import java.util.List;
 public class ReportGenerator {
     private String creekId;
 
-    public ReportGenerator(){
+    public ReportGenerator(){ //constructor
         this.creekId = null;
     }
 
+    /**
+     * Sets the ID of the closest creek.
+     *
+     * @param id The ID of the closest creek.
+     */
     public void setCreekId(String id){
         this.creekId = id;
     }
 
+    /**
+     * Generates and delivers a report based on the closest creek ID.
+     *
+     * @return A report indicating the closest creek ID, or a message indicating no creek was found.
+     */
     public String deliverReport(){
         return creekId == null ? "no creek found" : String.format("closest creek id: %s", creekId);
     }
